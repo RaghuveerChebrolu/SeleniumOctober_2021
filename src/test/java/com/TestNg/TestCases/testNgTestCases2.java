4efd71f4dc437572bce4d9f85b6f1877afa5099e
RaghuveerChebrolu <raghuveerchebrolu@gmail.com>
2021-11-09 00:02:13 +0530

    "08July2021 Selenium suite, Invocation count and navigation methods"

--- src/test/java/com/TestNg/TestCases/testNgTestCases2.java
+++ src/test/java/com/TestNg/TestCases/testNgTestCases2.java
@@ -17,12 +17,15 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -39,6 +42,7 @@ public class testNgTestCases2 {
 	@Test(priority = 0)
 	public void validtaeGMO_OnlineLaunchedSuccessfully() {
 		System.out.println("inside Test case validtaeGMO_OnlineLaunchedSuccessfully");
+		waitForPageToLoad();
 		String ActualTitle = driver.getTitle();
 		System.out.println("ActualTitle: " + ActualTitle);
 		String ExpectedTitle = "Welcome to Green Mountain Outpost";
@@ -47,14 +51,16 @@ public class testNgTestCases2 {
 	}
 
 	@Test(priority = 1, dependsOnMethods = { "validtaeGMO_OnlineLaunchedSuccessfully" })
-	public void validtaeEnterGMO_OnlineSuccessfully() {
+	public void validtaeEnterGMO_OnlineSuccessfully() throws InterruptedException {
 		System.out.println("inside Test case validtaeEnterGMO_OnlineSuccessfully");
 		driver.findElement(By.name("bSubmit")).click();
+		waitForPageToLoad();
 		String ActualTile = driver.findElement(By.xpath("//h1[contains(text(),'OnLine Catalog')]")).getText();
 		SoftAssert sa = new SoftAssert();
-		sa.assertEquals(ActualTile, "OnLine Catalo");
+		sa.assertEquals(ActualTile, "OnLine Catalog");
 		driver.findElement(By.xpath("//input[@name='QTY_BACKPACKS']")).sendKeys("4");
 		driver.findElement(By.xpath("//input[@value='Place An Order']")).click();
+		waitForPageToLoad();
 		String innerhtml = driver.findElement(By.xpath("//p[@align='center']")).getAttribute("innerHTML");
 		String outhtml = driver.findElement(By.xpath("//p[@align='center']")).getAttribute("outerHTML");
 		System.out.println(innerhtml);
@@ -74,7 +80,15 @@ public class testNgTestCases2 {
 		float ExpectedPrice = Float.parseFloat(Expectedvalue);
 		Assert.assertEquals(TotalpriceExternalFrameBack, ExpectedPrice);
 		sa.assertAll();
+		driver.navigate().refresh();
+		Thread.sleep(8000);
+		driver.navigate().back();
+		Thread.sleep(8000);
+		driver.navigate().forward();
+		Thread.sleep(8000);
 	}
+	
+	
 
 	@BeforeMethod
 	public void beforeMethod() {
@@ -148,8 +162,8 @@ public class testNgTestCases2 {
 			ObjProp.load(ObjFileInput);
 			ObjProp.getProperty("browser");
 			System.out.println(ObjProp.getProperty("browser"));
-			ObjProp.getProperty("GMO_Onlie");
-			System.out.println(ObjProp.getProperty("GMO_Onlie"));
+			ObjProp.getProperty("GMO_Online");
+			System.out.println(ObjProp.getProperty("GMO_Online"));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -164,9 +178,12 @@ public class testNgTestCases2 {
 			WebDriverManager.firefoxdriver().setup(); 
 			driver = new FirefoxDriver();
 			break;
-		case "chome":
+		case "chrome":
 			WebDriverManager.chromedriver().setup(); 
 			driver = new ChromeDriver();
+			//ChromeOptions objChromeOptions = new ChromeOptions();
+			//objChromeOptions.setAcceptInsecureCerts(true);
+			//driver= new ChromeDriver(objChromeOptions);
 			break;
 		case "edge":
 			WebDriverManager.edgedriver().setup(); 
@@ -180,10 +197,26 @@ public class testNgTestCases2 {
 			WebDriverManager.operadriver().setup();
 			driver=new OperaDriver();
 		}
-		driver.get(ObjProp.getProperty("GMO_Onlie"));
+		driver.get(ObjProp.getProperty("GMO_Online"));
+		//driver.get("https://demo.borland.com/gmopost/");
 		driver.manage().window().maximize();
+		/*implicit wait : It is a global waiting mechanism which is applicable for all web
+		elements that are declared under this statatement.
+		If webelement is identifed within less time than specified then it is going to access next webelement
+		*/
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS); 
 		
+	}
+	
+	public void waitForPageToLoad() {
+		ExpectedCondition<Boolean> pageLoadCondition = new ExpectedCondition<Boolean>() {
+			public Boolean apply(WebDriver driver) {
+				return ((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete");
+			}
+		};
+		// explicit wait -> Applicable for one webEllement
+		WebDriverWait wait = new WebDriverWait(driver, 60);//60 seconds 
+		wait.until(pageLoadCondition);
 	}
 
 
