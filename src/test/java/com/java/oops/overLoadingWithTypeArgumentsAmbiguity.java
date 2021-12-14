434e626cd79d06b2d6bbfe9b4cae420f85f5dfd6
RaghuveerChebrolu <raghuveerchebrolu@gmail.com>
2021-12-14 11:37:00 +0530

    13Dec2021 java oops

---
+++ src/test/java/com/java/oops/overLoadingWithTypeArgumentsAmbiguity.java
@@ -0,0 +1,20 @@
+package com.java.oops;
+
+class overLoadingWithTypeArgumentsAmbiguity {
+	void sum(int a, long b) {
+		System.out.println("a method invoked");
+		System.out.println(a+b);
+	}
+
+	void sum(long a, int b) {
+		System.out.println("b method invoked");
+	}
+
+	public static void main(String args[]) {
+		overLoadingWithTypeArgumentsAmbiguity obj = new overLoadingWithTypeArgumentsAmbiguity();
+		//obj.sum(20, 20);// now ambiguity because int type is promoted to long which is used in both sum methods
+	}
+}
+
+//Note : One type is not de-promoted implicitly for example 
+//double cannot be depromoted to any type implicitly.
